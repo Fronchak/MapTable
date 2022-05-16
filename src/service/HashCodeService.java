80df17c625f1b96732db2e4d2aaf7fe8d59aae9b
Gabriel Fronchak <gabrielgmack@gmail.com>
2022-05-15 22:12:05 -0300

    Create interface and class for hashCode

---
+++ src/service/HashCodeService.java
@@ -0,0 +1,7 @@
+package service;
+
+public interface HashCodeService {
+
+	int hashCode(Object obj);
+	
+}
---
+++ src/service/HashCodeServiceDjb2.java
@@ -0,0 +1,31 @@
+package service;
+
+public class HashCodeServiceDjb2 implements HashCodeService{
+
+	private int size;
+	
+	public HashCodeServiceDjb2(int size) {
+		this.size = size;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	@Override
+	public int hashCode(Object obj) {
+		String objString = obj.toString();
+		int hash = 5381;
+		for(int i = 0; i < objString.length(); i++) {
+			hash = (hash * 33) + ((Character)objString.charAt(i)).hashCode();
+		}
+		return hash % size;
+	}
+	
+	
+
+}
